7330de41dd6113d62fdfec9d850603c2c89f881d
--global <--global>
2018-11-23 20:55:43 -0300

    Agregando el front-end build

---
+++ client/build/precache-manifest.830196b72407a34ad4a93fddc989f932.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2663fb2f54af8f347034",
+    "url": "/static/js/main.2663fb2f.chunk.js"
+  },
+  {
+    "revision": "a6fda990d63f03ecd63b",
+    "url": "/static/js/1.a6fda990.chunk.js"
+  },
+  {
+    "revision": "2663fb2f54af8f347034",
+    "url": "/static/css/main.6c570af1.chunk.css"
+  },
+  {
+    "revision": "a6fda990d63f03ecd63b",
+    "url": "/static/css/1.23288268.chunk.css"
+  },
+  {
+    "revision": "3a494a73edfbdea64b29657b9d3fd55b",
+    "url": "/index.html"
+  }
+];
